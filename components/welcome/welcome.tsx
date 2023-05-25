0c278f267c908892367b5246c88bd0899b7152d4
FennStatistics <fenn.statistics@posteo.de>
2023-05-25 19:07:37 +0200

    changed welcome page

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -10,7 +10,6 @@ import {
     ThemeIcon,
 } from '@mantine/core';
 import { IconCheck } from '@tabler/icons';
-import image from '../../public/logoCAM2.svg';
 
 const useStyles = createStyles((theme) => ({
     inner: {
@@ -20,7 +19,7 @@ const useStyles = createStyles((theme) => ({
         paddingBottom: theme.spacing.xl * 2,
     },
 
-    content: {
+    contentNextImage: {
         maxWidth: 480,
         marginRight: theme.spacing.xl * 3,
 
@@ -30,10 +29,23 @@ const useStyles = createStyles((theme) => ({
         },
     },
 
+
+    contentBelowImage: {
+        //marginLeft: 100,
+        maxWidth: 960,
+        marginRight: theme.spacing.xl * 3,
+
+        [theme.fn.smallerThan('md')]: {
+            maxWidth: '100%',
+            marginRight: 0,
+        },
+    },
+
+    
     title: {
         color: theme.colorScheme === 'dark' ? theme.white : theme.black,
         fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-        fontSize: 44,
+        fontSize: 40,
         lineHeight: 1.2,
         fontWeight: 900,
 
@@ -54,13 +66,16 @@ const useStyles = createStyles((theme) => ({
         [theme.fn.smallerThan('md')]: {
             display: 'none',
         },
+        marginTop: 'auto',
+        marginBlockEnd: 'auto'
     },
 
     highlight: {
+        fontSize: 46,
         position: 'relative',
         //backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
         borderRadius: theme.radius.sm,
-        padding: '4px 12px',
+        // padding: '4px 12px',
     },
 }));
 
@@ -70,16 +85,19 @@ export function HeroBullets() {
         <div>
             <Container>
                 <div className={classes.inner}>
-                    <div className={classes.content}>
+                    <div className={classes.contentNextImage}>
                         <Title className={classes.title}>
-                            A <span className={classes.highlight}>Cognitive Affective Map tool</span> to easily create and integrate CAM studies into online experiments.
+                            <span className={classes.highlight}>Cognitive Affective Map tools</span> to easily create and integrate CAM studies into online experiments.
                         </Title>
                         <Text color="dimmed" mt="md">
                             Build fully functional accessible web applications faster than ever – Mantine includes
                             more than 120 customizable components and hooks to cover you in any situation
                         </Text>
-
-                        <List
+                    </div>
+                </div>
+                <div className={classes.inner}>
+                <div className={classes.contentBelowImage}>
+                           <List
                             mt={30}
                             spacing="sm"
                             size="sm"
@@ -89,7 +107,7 @@ export function HeroBullets() {
                                 </ThemeIcon>
                             }
                         >
-                              <List.Item>
+                            <List.Item>
                                 <b>Online based</b> – all our tools are running online so no need to setup any server (although you could)
                             </List.Item>
                             <List.Item>
@@ -114,8 +132,7 @@ export function HeroBullets() {
                             </a>
                         </Group>
                     </div>
-                    <Image src={image.src} className={classes.image} />
-                </div>
+                    </div>
             </Container>
         </div>
     );
