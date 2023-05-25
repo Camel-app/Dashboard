9149d71ebb9c6d212706ab6cb748a353d3bb420c
FennStatistics <fenn.statistics@posteo.de>
2023-05-25 19:14:29 +0200

    changed welcome page

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -91,8 +91,7 @@ export function HeroBullets() {
                             <span className={classes.highlight}>Cognitive Affective Map tools</span> to easily create and integrate CAM studies into online experiments.
                         </Title>
                         <Text color="dimmed" mt="md">
-                            Build fully functional accessible web applications faster than ever – Mantine includes
-                            more than 120 customizable components and hooks to cover you in any situation
+                            Build fully functional accessible Cognitive Affective Map experiments faster than ever.
                         </Text>
                     </div>
                     <Image src={image.src} className={classes.image} />
