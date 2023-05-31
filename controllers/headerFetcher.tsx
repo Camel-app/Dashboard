88e1950d851c04bef70b9b8e4752a5a7c78d62da
FennStatistics <fenn.statistics@posteo.de>
2023-05-31 12:20:33 +0200

    try to fix bug (logout not always working - API?)

--- controllers/headerFetcher.tsx
+++ controllers/headerFetcher.tsx
@@ -5,7 +5,7 @@ export default function getNavbar(isLoggedin: boolean) {
             { link: 'https://camtools-documentation.readthedocs.io/en/master/', label: 'Documentation', target: "_blank"},
             { link: 'https://camgalaxy.github.io/', label: 'C.A.M.E.L.', target: "_blank"},
             { link: 'https://fennapps.shinyapps.io/shinyCAMEL_v02/', label: 'CAM-App', target: "_blank"},
-            { link: '/logout', label: 'Log out', target: "_self"}
+            { link: '/logout', label: 'Log out', target: "_self"},
         ];
     }
     return [
