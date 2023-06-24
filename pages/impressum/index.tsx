9cc3bcaa03e362ebcf7dbf56817c14eb521f63bf
FennStatistics <fenn.statistics@posteo.de>
2023-06-24 13:15:51 +0200

    removed not needed imports

--- pages/impressum/index.tsx
+++ pages/impressum/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 
-import HeaderSimple from '../../components/header/Header';
 import Impressum from "../../components/impressum/Impressum";
-import getNavbar from '../../controllers/headerFetcher';
 
 
 const impressum: NextPage = () => {
--- pages/information/index.tsx
+++ pages/information/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 
-import HeaderSimple from '../../components/header/Header';
 import Information from "../../components/information/Information";
-import getNavbar from '../../controllers/headerFetcher';
 
 const getInformation: NextPage = () => {
 
