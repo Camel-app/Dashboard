2d89e00abccdb07060a1d494fe3c5f5ff4a3944a
FennStatistics <fenn.statistics@posteo.de>
2023-06-26 09:45:20 +0200

    stats number nodes, avg valence

--- components/metrics/Stats.tsx
+++ components/metrics/Stats.tsx
@@ -2,7 +2,30 @@ import { Title, Text, Table } from '@mantine/core';
 
 export function Metrics({ data }) {
 
-    console.log(data.daughters[0]);
+    console.log("data.daughters[0]:", data.daughters[0]);
+
+
+    // get all values of all nodes
+    var arrayValuesNodes = [];
+
+    console.log("data.daughters:", data.daughters);
+    data.daughters.forEach(daughter => {
+        //console.log("daughter", daughter)
+        
+        daughter.cam.nodes.forEach(element => {
+            if (element.value === 10) {
+                // replace 10 (ambivalent) by 0
+                arrayValuesNodes.push(0);
+            }else{
+                arrayValuesNodes.push(element.value);
+            }
+        });
+    });
+   
+    // compute sum, average
+    var sumNodes = arrayValuesNodes.reduce((a, b) => a + b, 0);
+    var avgNodes = (sumNodes / arrayValuesNodes.length) || 0;
+    console.log(`The sum is: ${sumNodes}. The average is: ${avgNodes}.`);
 
     return (
         <div style={{
@@ -28,7 +51,7 @@ export function Metrics({ data }) {
                 width: 250,
                 borderRadius: 5
             }}>
-                <Title >432</Title>
+                <Title >{arrayValuesNodes.length}</Title>
                 <Text style={{
                     fontStyle: "italic",
                     fontSize: 20
@@ -41,7 +64,7 @@ export function Metrics({ data }) {
                 width: 250,
                 borderRadius: 5
             }}>
-                <Title style={{ "color": "red" }}>-0.31</Title>
+                <Title style={{ "color": avgNodes > 0 ? "green" : "red" }}>{avgNodes.toFixed(2)}</Title>
                 <Text style={{
                     fontStyle: "italic",
                     fontSize: 20
@@ -50,3 +73,5 @@ export function Metrics({ data }) {
         </div>
     );
 }
+
+
