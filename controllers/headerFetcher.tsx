a1e53f923b69c7b87656eeb9ad652347c9cc257a
Julius Fenn <camel.app@proton.me>
2023-08-22 16:32:23 +0200

    update links

--- controllers/headerFetcher.tsx
+++ controllers/headerFetcher.tsx
@@ -2,7 +2,7 @@ export default function getNavbar(isLoggedin: boolean) {
     if (isLoggedin) {
         return [
             { link: '/dashboard', label: 'Dashboard', target: "_self"},
-            { link: 'https://camtools-documentation.readthedocs.io/en/master/', label: 'Documentation', target: "_blank"},
+            { link: 'https://osf.io/q5hj4/', label: 'Documentation', target: "_blank"},
             { link: 'https://camgalaxy.github.io/static/?ShowResearcherButtons=true&fullScreen=false', label: 'C.A.M.E.L. (researcher view)', target: "_blank"},
             { link: 'https://fennapps.shinyapps.io/CAMtools_CAMapp/', label: 'CAM-App', target: "_blank"},
             { link: '/logout', label: 'Log out', target: "_self"},
@@ -10,7 +10,7 @@ export default function getNavbar(isLoggedin: boolean) {
     }
     return [
         { link: '/', label: 'Home' },
-        { link: 'https://camtools-documentation.readthedocs.io/en/master/', label: 'Documentation', target: "_blank"},
+        { link: 'https://osf.io/q5hj4/', label: 'Documentation', target: "_blank"},
         { link: 'https://camgalaxy.github.io/static/', label: 'C.A.M.E.L. (participant view)', target: "_blank"},
         { link: 'https://fennapps.shinyapps.io/CAMtools_CAMapp/', label: 'CAM-App', target: "_blank"},
         { link: '/apply', label: 'Apply account', target: "_self"},
